56c1a3342be5f9a53bcd090cea4b3617ff8a3a6c
Komal Sharma <ikomalsharma98@gmail.com>
2026-03-29 01:24:35 +0530

    feat: Add download, list and delete expense and income features

---
+++ frontend/expense-tracker/src/components/Expense/ExpenseList.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { LuDownload } from 'react-icons/lu'
+import TransactionInfoCard from '../Cards/TransactionInfoCard'
+import moment from 'moment'
+
+const ExpenseList = ({ transactions, onDelete, onDownload }) => {
+    return (
+        <div className='card' >
+            <div className=' flex items-center justify-between'>
+                <h5 className=' text-lg '>
+                    All Expenses
+                </h5>
+
+                <button onClick={onDownload} className=' card-btn'>
+                    <LuDownload className=' text-base' />
+                    Download
+                </button>
+
+            </div>
+
+            <div className=' grid grid-cols-1 md:grid-cols-2 gap-x-4'>
+                {transactions.map((expense) => (
+                    <TransactionInfoCard
+                        key={expense._id}
+                        title={expense.category}
+                        amount={expense.amount}
+                        date={moment(expense.date).format('Do MMM YYYY')}
+                        icon={expense.icon}
+                        type={"expense"}
+                        onDelete={() => onDelete(expense._id)}
+                    />
+                ))}
+            </div>
+
+        </div>
+    )
+}
+
+export default ExpenseList
